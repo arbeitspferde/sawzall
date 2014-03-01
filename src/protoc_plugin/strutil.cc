a0048683dafdefcc5c125af26a94aa71875ce1a7
Matt T. Proud <matt.proud@gmail.com>
2014-03-01 22:59:30 +0100

    Fix numeric stringification in generated code.
    The original code possesses a problem whereby the stringification of
    numeric values in generated code can overrun the allotted buffer in
    the inlined stringification helper function.
    
    Namely, the following scenario yields a segmentation fault:
    
        // foo.proto
        message Foo {
          optional int32 some_field = 1 [default=1000];
        }
    
        # compiling
        $ protoc --szl_out=. foo.proto
    
    This is because (len("2147483647") / 10) + 3 = 4, the default buffer
    size for int32, is insufficient for sprintf(buf, fmt, value) of 1000
    plus the null terminator.  I presume this was just an oversight.

--- src/protoc_plugin/strutil.cc
+++ src/protoc_plugin/strutil.cc
@@ -1,11 +1,11 @@
 // Copyright 2010 Google Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,7 +38,7 @@ template<int I> struct CompileTimeLog10 : CTL10Helper<I,0> { };
 
 template<typename T>
 inline string CommonItoa(T t, const char* fmt) {
-  char buffer[CompileTimeLog10<numeric_limits<T>::digits10>::value + 3];
+  char buffer[numeric_limits<T>::digits10 + 3];
   sprintf(buffer, fmt, t);
   return buffer;
 }
